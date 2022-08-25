4af75d60ed5f023c665f754b8c965d3bc612ce36
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-25 11:34:44 +0200

    zadanie 7

--- NUnitForTesters.Test/Services/CollectionServiceTests.cs
+++ NUnitForTesters.Test/Services/CollectionServiceTests.cs
@@ -6,5 +6,14 @@ namespace NUnitForTesters.Test.Services
 {
     public class CollectionServiceTests
     {
-    }
+        [Test]
+        public void GetInitialsFromNamesConstructorTest()
+        {
+            var service = new CollectionService();
+
+			CollectionAssert.IsNotEmpty(service.Names);
+            CollectionAssert.AllItemsAreNotNull(service.Names);
+			CollectionAssert.AllItemsAreUnique(service.Names);
+        }
+	}
 }
