b75a6034ef66bba4b328738351bd68c48d0d053d
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-25 11:00:58 +0200

    zadanie 2

--- NUnitForTesters.Test/Parallel/HtmlTextFormatterTests.cs
+++ NUnitForTesters.Test/Parallel/HtmlTextFormatterTests.cs
@@ -7,5 +7,73 @@ using System.Threading;
 
 namespace NUnitForTesters.Test.Parallel
 {
+	[Parallelizable(ParallelScope.Children)]
+	public class HtmlTextFormatterTests
+	{
+		[TestCase("zbyszek")]
+		[TestCase("ala")]
+		[TestCase("ma")]
+		[TestCase("kota")]
+		public void FormatAsBoldTest(string text)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<b>{text}</b>", formatter.FormatAsBold(text));
+		}
 
+		[TestCase("zbyszek")]
+		[TestCase("ala")]
+		[TestCase("ma")]
+		[TestCase("kota")]
+		public void FormatAsItalicTest(string text)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<i>{text}</i>", formatter.FormatAsItalic(text));
+		}
+
+		[TestCase("zbyszek")]
+		[TestCase("ala")]
+		[TestCase("ma")]
+		[TestCase("kota")]
+		public void FormatAsSmallTest(string text)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<small>{text}</small>", formatter.FormatAsSmall(text));
+		}
+
+		[TestCase("zbyszek")]
+		[TestCase("ala")]
+		[TestCase("ma")]
+		[TestCase("kota")]
+		public void FormatAsDeletedTest(string text)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<del>{text}</del>", formatter.FormatAsDeleted(text));
+		}
+
+		[TestCase("zbyszek")]
+		[TestCase("ala")]
+		[TestCase("ma")]
+		[TestCase("kota")]
+		public void FormatAsSubscriptTest(string text)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<sub>{text}</sub>", formatter.FormatAsSubscript(text));
+		}
+
+		[TestCase("zbyszek", "picca")]
+		[TestCase("ala", "kota")]
+		[TestCase("ma", "aka")]
+		[TestCase("kota", "ma")]
+		public void FormatAsCustomTagTest(string text, string customTag)
+		{
+			var formatter = new HtmlTextFormatter();
+			StringAssert.IsMatch($"<{customTag}>{text}</{customTag}>", formatter.FormatAsCustomTag(text, customTag));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			Thread.Sleep(1500);
+		}
+	}
 }
