e5d6bf3c6435396a8525076b07be024a1c336251
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-25 11:08:53 +0200

    zadanie 3

--- NUnitForTesters.Test/Services/NumberServiceTests.cs
+++ NUnitForTesters.Test/Services/NumberServiceTests.cs
@@ -5,5 +5,12 @@ namespace NUnitForTesters.Test.Services
 {
     public class NumberServiceTests
     {
-    }
+        // 0 & 50 fail test
+        [Test]
+        public void IsItTheAnswerToUltimateQuestionOfLifeTest([Range(-10, 110)] int number)
+        {
+            var numService = new NumberService();
+            StringAssert.DoesNotMatch("That should not happen at all", numService.IsItTheAnswerToUltimateQuestionOfLife(number), "Number: {0}", number);
+        }
+	}
 }
