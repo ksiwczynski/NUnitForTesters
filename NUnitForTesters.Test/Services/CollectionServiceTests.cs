77ab24455fb3f37b8a4f2755ed70ca058a725a33
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-25 11:43:06 +0200

    zadanie 8

--- NUnitForTesters.Test/Services/CollectionServiceTests.cs
+++ NUnitForTesters.Test/Services/CollectionServiceTests.cs
@@ -15,5 +15,20 @@ namespace NUnitForTesters.Test.Services
             CollectionAssert.AllItemsAreNotNull(service.Names);
 			CollectionAssert.AllItemsAreUnique(service.Names);
         }
+
+        [Test]
+        public void GetInitialsFromNamesCustomTest()
+        {
+            var customNames = new List<string> { "Konrad Siwczyński", "Przemysław Kardas" };
+            var initials = new List<string> { "KS", "PK" };
+
+            var service = new CollectionService(customNames);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(customNames.Count, Is.EqualTo(initials.Count));
+                CollectionAssert.AreEqual(initials, service.GetInitialsFromNames());
+            });
+        }
 	}
 }
